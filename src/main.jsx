5f3b9f7b54aaa8c54779e95057508ada64027c1b
Louis <poostmaan@gmail.com>
2023-04-25 21:06:41 -0400

    basename routes

--- src/main.jsx
+++ src/main.jsx
@@ -12,7 +12,7 @@ import './index.css'
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={"/heroes-wiki"}>
       <HeroesApp />
     </Router>
   </React.StrictMode>
